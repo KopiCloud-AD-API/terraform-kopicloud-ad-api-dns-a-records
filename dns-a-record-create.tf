6ba44d251d2b8d13c4a9b918386c9acc00b18df3
Guillermo Musumeci <guillermo-musumeci@users.noreply.github.com>
2023-02-11 12:09:40 +0100

    Initial commit

---
+++ dns-a-record-create.tf
@@ -0,0 +1,21 @@
+#############################################
+## KopiCloud AD API - Create DNS A Records ##
+#############################################
+
+# Create a DNS A Record for a computer
+resource "kopicloud_dns_a_record" "test_a" {
+  hostname   = "atftestvm01"
+  ip_address = "100.30.1.1"
+  zone_name  = "kopicloud.local"
+}
+
+# Output Created DNS A Record 
+output "dns_a_record" {
+  description = "Created DNS A Record"
+  value       = resource.kopicloud_dns_a_record.test_a
+}
+
+output "dns_a_record_hostname" {
+  description = "Hostname of Created DNS A Record"
+  value       = resource.kopicloud_dns_a_record.test_a.hostname
+}
